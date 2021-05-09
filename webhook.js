eb45e75da3af175c829110ed7ca6cab78d5f4045
shenshuai <shenshuai89@qq.com>
2021-05-09 15:05:48 +0800

    update

--- webhook.js
+++ webhook.js
@@ -2,7 +2,7 @@ const HTTP = require('http')
 const server = HTTP.createServer((req, res) => {
   console.log(req.method, req.url);
   if(req.method == 'POST' && req.url == '/webhook'){
-    res.setHeaders('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ok:true}));
   }else{
     res.end('Not Found');
