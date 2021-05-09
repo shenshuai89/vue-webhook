b0c78f98e0a0bbfb788230a6250932af704bdafb
shenshuai <shenshuai89@qq.com>
2021-05-09 15:29:24 +0800

    设置sign方法

--- webhook.js
+++ webhook.js
@@ -1,7 +1,27 @@
 const HTTP = require('http')
+const crypto = require('crypto')
+const SECRET = '123456'
+function sign(body){
+  return `sha1=`+crypto.createHmac('sha1',SECRET).update(body).digest('hex')
+}
 const server = HTTP.createServer((req, res) => {
   console.log(req.method, req.url);
   if(req.method == 'POST' && req.url == '/webhook'){
+    // 监听github hook返回的数据
+    let buffers = []
+    req.on('data',(buffer) =>{
+      buffers.push(buffer)
+    })
+    req.on('end',() =>{
+      let body = Buffer.concat(buffers);
+      // 判断event：push
+      let event = req.headers['x-github-event']; 
+      // github请求来的时候，要传递请求体body，另外在传一个signature
+      let signature = req.headers['x-hub-signature'];
+      if(signature !== sign(body)){
+        return res.end('Not Allowed');
+      }
+    })
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ok:true}));
   }else{
